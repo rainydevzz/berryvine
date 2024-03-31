0a62b3ffe5c40dee9aa66d7c877b49f6a2078b9d
rainydevzz <ryan.skeeb@gmail.com>
2024-03-30 20:48:21 -0700

    await glob

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import { html } from '@elysiajs/html';
 import { Elysia } from 'elysia';
 import hbs from 'handlebars';
-import { globSync } from 'glob';
+import { glob } from 'glob';
 import { db } from '../migrate';
 import { metadata } from '../schema';
 import { staticPlugin } from '@elysiajs/static';
@@ -12,9 +12,9 @@ export const index = new Elysia()
     .get('/', async () => {
         let contents = await Bun.file('./static/index.hbs').text();
         let render = hbs.compile(contents);
-        let files = globSync("**", {ignore: "node_modules/**"}).filter(f => f.startsWith("files") && f.includes("/"));
+        let files = (await glob("**", {ignore: "node_modules/**"})).filter(f => f.startsWith("files") && f.includes("/"));
         let fileArr = [];
-        let names = await db.select().from(metadata).all();
+        let names = db.select().from(metadata).all();
         if(names.length > 0) {
           for(const i in files) {
             fileArr.push({file: files[i], name: names[i].name, id: names[i].id});
