efcec1cc97c2b54b27143ca0b5a69c5826f1b221
rainydevzz <ryan.skeeb@gmail.com>
2024-03-31 17:09:48 -0700

    possible video fix

--- src/routes/shareLink.ts
+++ src/routes/shareLink.ts
@@ -9,12 +9,15 @@ import staticPlugin from "@elysiajs/static";
 export const shareLinks = new Elysia()
     .use(html())
     .use(staticPlugin({assets: './files', alwaysStatic: false, indexHTML: true, prefix: '/files'}))
-    .get('/share-link/:id', async ({ params: { id } }) => {
+    .get('/share-link/:id', async ({ params: { id }, request }) => {
+        let urlVar = 'https://' + new URL(request.url).host + `/files/${id}.mp4`;
+        console.log(urlVar);
         let clips = await db.select().from(metadata).where(eq(metadata.id, id));
         let toShare = clips[0];
         let render = hbs.compile(await Bun.file('./static/link.hbs').text());
         return render({
             name: `${toShare.name}`,
-            src: toShare.path
+            src: toShare.path,
+            urlvar: urlVar
         })
     });
